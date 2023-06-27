bbd6910c28c533d0bc988a77c2f4cd1a3418a1d2
KONG JIA ROU <120616141+jrkong2001utm@users.noreply.github.com>
2023-06-28 02:26:35 +0800

    Update report.blade.php

--- eis/resources/views/report.blade.php
+++ eis/resources/views/report.blade.php
@@ -1,36 +1,5 @@
 @extends('template')
 @section('report')
-
-<!DOCTYPE html>
-<html lang="en">
-  <!--begin::Head-->
-  <head><base href=""/>
-    <title>Metronic - the world's #1 selling Bootstrap Admin Theme Ecosystem for HTML, Vue, React, Angular & Laravel by Keenthemes</title>
-    <meta charset="utf-8" />
-    <meta name="description" content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Blazor, Django, Flask & Laravel versions. Grab your copy now and get life-time updates for free." />
-    <meta name="keywords" content="metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Blazor, Django, Flask & Laravel starter kits, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta property="og:locale" content="en_US" />
-    <meta property="og:type" content="article" />
-    <meta property="og:title" content="Metronic | Bootstrap HTML, VueJS, React, Angular, Asp.Net Core, Blazor, Django, Flask & Laravel Admin Dashboard Theme" />
-    <meta property="og:url" content="https://keenthemes.com/metronic" />
-    <meta property="og:site_name" content="Keenthemes | Metronic" />
-    <link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
-    <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
-    <!--begin::Fonts-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
-    <!--end::Fonts-->
-    <!--begin::Vendor Stylesheets(used by this page)-->
-    <link href="assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css" />
-    <link href="assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
-    <!--end::Vendor Stylesheets-->
-    <!--begin::Global Stylesheets Bundle(used by all pages)-->
-    <link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
-    <!--end::Global Stylesheets Bundle-->
-  </head>
-  <!--end::Head-->
-  <!--begin::Body-->
  
   <style>
 body {font-family: Arial, Helvetica, sans-serif;}
@@ -103,17 +72,18 @@ tr:hover {background-color: #d6d4d4;}
 
 </style>
 
-<body>
-    <div class="container">
-
-
-        <!-- Filter -->
-        <!-- For sort weekly -->
-        <div class="">
-        <div class="col-9 grid-margin">
-        <div class="card">
-        <div class="card-body">
-        <div class="d-flex justify-content-between">
+<!--begin::Content-->
+<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+    <!--begin::Container-->
+    <div class="container-xxl" style="width: 1000px" id="kt_content_container">
+    
+    <div class="row">
+    <div class="col-md-12 grid-margin transparent">
+        <div class="row">
+            <div class="col-md-8 mb-4 stretch-card transparent">
+                <div class="card card-tale">
+                    <div class="card-body">
+                    <div class="d-flex justify-content-between">
         <p class="card-title mb-0" style="font-size: x-large; font-weight:bold">Product Sales List</p>
             <div class="ml-auto">
                 <button class="btn btn-success btn-sm mx-3" id="generateReportButton" onClick="PrintElem()">Generate Report</button>
@@ -170,13 +140,13 @@ tr:hover {background-color: #d6d4d4;}
     </tfoot>
 </table>
 </div>
-</div>
-</div>
-
-<div class="right-bar col-5 grid-margin px-0" style="width: 25vw;">
-<div class="card">
-<div class="card-body">
-<form action="{{ route('dateBetween') }}" method="GET">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4 stretch-card transparent">
+                <div class="card card-dark-blue">
+                    <div class="card-body">
+                    <form action="{{ route('dateBetween') }}" method="GET">
                 @csrf
                 <div class="form-group row">
                 <label class="col-sm-3 col-form-label" for="">Start Date</label>
@@ -208,29 +178,17 @@ tr:hover {background-color: #d6d4d4;}
                 <input type="submit" value="Filter" class="btn btn-primary btn-sm">
                 <a href="{{ route('report') }}" class="btn btn-warning btn-sm">Clear Filter</a>
 </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </div>
 </div>
 </div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+</div>
 
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <script src="{{ asset('js/table-sortable.js') }}" type="text/javascript"></script>
@@ -261,8 +219,5 @@ tr:hover {background-color: #d6d4d4;}
 
 </script>
 
-
-</body>
-</html>
 
 @endsection
