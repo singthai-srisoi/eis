bd08febb06014bd1c9b361932acc392408b26a3c
KONG JIA ROU <120616141+jrkong2001utm@users.noreply.github.com>
2023-06-28 02:27:47 +0800

    Update SalesController.php

--- eis/app/Http/Controllers/SalesController.php
+++ eis/app/Http/Controllers/SalesController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Models\Order_item;
 use App\Models\Order;
 use App\Models\Product;
+use Carbon\Carbon;
 
 class SalesController extends Controller
 {
@@ -18,14 +19,14 @@ class SalesController extends Controller
      */
     public function index()
     {
-        $orders = DB::table('orders')
-            ->join('order_items', 'orders.o_id', '=', 'order_items.oItem_orderId')
-            ->join('products', 'order_items.oItem_product', '=', 'products.prod_id')
-            ->where('orders.o_status', '=', 'PAID')
-            ->select('products.prod_code', 'products.prod_name', 'order_items.oItem_qty', 'orders.o_tax', 'orders.o_totalAmount', 'order_items.oItem_totalprice')
+        $orders = DB::table('tb_orders')
+            ->join('tb_order_items', 'tb_orders.o_id', '=', 'tb_order_items.oItem_orderId')
+            ->join('tb_products', 'tb_order_items.oItem_product', '=', 'tb_products.prod_id')
+            ->where('tb_orders.o_status', '=', 'PAID')
+            ->select('tb_products.prod_code', 'tb_products.prod_name', 'tb_order_items.oItem_qty', 'tb_orders.o_tax', 'tb_orders.o_totalAmount', 'tb_order_items.oItem_totalprice','tb_orders.o_id')
             ->get();
 
-        $distinctProducts = DB::table('products')
+        $distinctProducts = DB::table('tb_products')
             ->select('prod_code', 'prod_name')
             ->distinct()
             ->get();
@@ -36,90 +37,113 @@ class SalesController extends Controller
     public function dateBetween(Request $request){
         $selectedProducts = $request->input('products');
 
-        $orders = DB::table('orders')
-            ->join('order_items', 'orders.o_id', '=', 'order_items.oItem_orderId')
-            ->join('products', 'order_items.oItem_product', '=', 'products.prod_id')
-            ->where('orders.o_status', '=', 'PAID')
-            ->whereBetween('orders.o_dateIssued', [$request->startDate, $request->endDate])
-            ->select('products.prod_code', 'products.prod_name', 'order_items.oItem_qty', 'orders.o_tax', 'orders.o_totalAmount', 'order_items.oItem_totalprice')
-            ->get();
+
+        $orders = DB::table('tb_orders')
+                ->join('tb_order_items', 'tb_orders.o_id', '=', 'tb_order_items.oItem_orderId')
+                ->join('tb_products', 'tb_order_items.oItem_product', '=', 'tb_products.prod_id')
+                ->where('tb_orders.o_status', '=', 'PAID');
 
         if (!empty($selectedProducts)) {
-            $orders->whereIn('products.prod_code', $selectedProducts);
+            $orders->whereIn('tb_products.prod_code', $selectedProducts);
         }
 
-        $distinctProducts = DB::table('products')
+        if (!empty($request->startDate) && !empty($request->endDate)) {
+            $orders->whereBetween('tb_orders.o_dateIssued', [$request->startDate, $request->endDate]);
+        }
+
+        $orders = $orders->select('tb_products.prod_code', 'tb_products.prod_name', 'tb_order_items.oItem_qty', 'tb_orders.o_tax', 'tb_orders.o_totalAmount', 'tb_order_items.oItem_totalprice', 'tb_orders.o_id')
+            ->get();
+
+        $distinctProducts = DB::table('tb_products')
             ->select('prod_code', 'prod_name')
             ->distinct()
             ->get();
 
-        return view('report', ['orders' => $orders, 'distinctProducts' => $distinctProducts]);
+        return view('report', ['orders' => $orders, 'distinctProducts' => $distinctProducts, 'filterData' => $request]);
     }
 
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function dashboard()
     {
-        //
-    }
+        // for monthly sales and profit of each product
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        $currentYear = date('Y');
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Sale  $sale
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Sale $sale)
-    {
-        //
-    }
+        $orders = DB::table('tb_orders AS o')
+        ->join('tb_order_items AS oi', 'o.o_id', '=', 'oi.oItem_orderId')
+        ->join('tb_products AS prod', 'oi.oItem_product', '=', 'prod.prod_id')
+        ->select(
+            DB::raw('MONTH(o.o_dateIssued) AS month'),
+            'prod.prod_name AS product',
+            DB::raw('SUM(oi.oItem_qty * (prod.prod_sellprice * (1 + o.o_tax / 100))) AS total_sales'),
+            DB::raw('SUM(oi.oItem_qty * (prod.prod_sellprice - prod.prod_price)) AS total_profit'),
+            DB::raw('SUM(oi.oItem_qty) AS total_qty')
+        )
+        ->where('o.o_status', '=', 'Paid')
+        ->whereYear('o.o_dateIssued', '=', $currentYear)
+        ->groupBy('month', 'product')
+        ->orderBy('month')
+        ->get();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Sale  $sale
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Sale $sale)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Sale  $sale
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Sale $sale)
-    {
-        //
-    }
+        // get weekly profit
+        $currentDate = Carbon::now();
+        $startDate = $currentDate->startOfWeek()->toDateString(); // Start date of the week
+        $endDate = $currentDate->endOfWeek()->toDateString();     // End date of the week
+        
+        $weeklyProfit = DB::table('tb_orders AS o')
+                ->join('tb_order_items AS oi', 'o.o_id', '=', 'oi.oItem_orderId')
+                ->join('tb_products AS prod', 'oi.oItem_product', '=', 'prod.prod_id')
+                ->select(
+                    DB::raw('WEEK(o.o_dateIssued) AS week'),
+                    DB::raw('SUM(oi.oItem_qty * (prod.prod_sellprice - prod.prod_price)) AS weekly_profit')
+                )
+                ->where('o.o_status', '=', 'Paid')
+                ->whereYear('o.o_dateIssued', '=', 2023)
+                ->whereBetween('o.o_dateIssued', [$startDate, $endDate])
+                ->groupBy('week')
+                ->get();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Sale  $sale
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Sale $sale)
-    {
-        //
+
+        // top three products
+        $topThreeProducts = DB::table('tb_orders AS o')
+                ->join('tb_order_items AS oi', 'o.o_id', '=', 'oi.oItem_orderId')
+                ->join('tb_products AS prod', 'oi.oItem_product', '=', 'prod.prod_id')
+                ->select(
+                    'prod.prod_name AS product',
+                    DB::raw('SUM(oi.oItem_qty) AS total_qty')
+                )
+                ->where('o.o_status', '=', 'Paid')
+                ->whereYear('o.o_dateIssued', '=', 2023)
+                ->groupBy('product')
+                ->orderBy('total_qty', 'DESC')
+                ->limit(3)
+                ->get();
+                    
+        // monthly data
+        $currentMonth = date('n');
+
+        $monthlyData = DB::table('tb_orders AS o')
+            ->join('tb_order_items AS oi', 'o.o_id', '=', 'oi.oItem_orderId')
+            ->join('tb_products AS prod', 'oi.oItem_product', '=', 'prod.prod_id')
+            ->select(
+                DB::raw('MONTH(o.o_dateIssued) AS month'),
+                DB::raw('SUM(oi.oItem_qty * (prod.prod_sellprice * (1 + o.o_tax / 100))) AS total_sales'),
+                DB::raw('SUM(oi.oItem_qty * (prod.prod_sellprice - prod.prod_price)) AS total_profit'),
+                DB::raw('((SUM(oi.oItem_qty * (prod.prod_sellprice - prod.prod_price)) - 
+                    LAG(SUM(oi.oItem_qty * (prod.prod_sellprice - prod.prod_price))) 
+                    OVER (ORDER BY MONTH(o.o_dateIssued))) / 
+                    LAG(SUM(oi.oItem_qty * (prod.prod_sellprice - prod.prod_price))) 
+                    OVER (ORDER BY MONTH(o.o_dateIssued))) * 100 AS profit_growth'),
+                DB::raw('SUM(oi.oItem_qty) AS total_order')
+            )
+            ->where('o.o_status', '=', 'Paid')
+            ->groupBy('month')
+            ->orderBy('month')
+            ->get();
+        $monthlyData = $monthlyData->whereMonth('o.o_dateIssued', '=', $currentMonth);
+
+        return view('sales_dashboard', ['orders' => $orders, 'weeklyProfit' => $weeklyProfit, 'topThreeProducts' => $topThreeProducts, 'monthlyData' => $monthlyData]);
     }
 }
+
